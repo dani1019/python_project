43badc98472fbb1def449f848db559e7a1a92873
dani1019 <nihonkita17@gmail.com>
2022-10-13 05:34:23 +0900

    the game of rock_paper_scissors

--- game/rock_paper_scissors_game/main.py
+++ game/rock_paper_scissors_game/main.py
@@ -1,11 +1,20 @@
 import picture
+import random
 
 #mark quesiton for user to enter the number
 input("What do  you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors ")
 
 #process random selection for computer's
+list_thing = []
+list_thing.append(picture.rock)
+list_thing.append(picture.paper)
+list_thing.append(picture.scissors)
+
+random_select = random.choice(list_thing)
+print(random_select)
 
 #judge whether if user win
+#rock 0, paper 1, scissors 2
 
 #print thing user selecting, computer's
 
