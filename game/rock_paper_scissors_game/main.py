16cd2feffa48e0852817e4929955c387b1f22b7a
dani1019 <nihonkita17@gmail.com>
2022-10-14 05:40:33 +0900

    update the game of scissors

--- game/rock_paper_scissors_game/main.py
+++ game/rock_paper_scissors_game/main.py
@@ -2,7 +2,7 @@ import picture
 import random
 
 #mark quesiton for user to enter the number
-input("What do  you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors ")
+select_num =  int(input("What do  you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
 
 #process random selection for computer's
 list_thing = []
@@ -14,6 +14,17 @@ random_select = random.choice(list_thing)
 print(random_select)
 
 #judge whether if user win
+#rock 0 < paper 1
+#rock 0 > scissors 2
+#paper 1 < scissors 2
+if select_num > random_select:
+    if(select_num == 0) and (random_select == 2):
+        print("You lose.")
+    else:
+        print("You win.")
+else:
+    print("You lose.")
+        
 #rock 0, paper 1, scissors 2
 
 #print thing user selecting, computer's
