5a2c4a744937d600cc85537174d579c8a8a530ee
dani1019 <nihonkita17@gmail.com>
2022-11-01 04:56:47 +0900

    make the create_password

---
+++ game/create_password/main.py
@@ -0,0 +1,33 @@
+import letter
+import random
+
+print("Welcome to the PyPassword Generator!")
+
+#enter the number of letters
+nr_letters= int(input("How many letters would you like in your password?\n"))
+
+#select the letter that user enters the number 
+letter_list = random.choices(letter.letters, k = nr_letters)
+
+#enter the number of sumbols
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+
+#select the symbols that user enters the number
+symbol_list = random.choices(letter.symbols, k = nr_symbols)
+
+#enter the number of numbers
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#select the numbers that user enters the number
+number_list = random.choices(letter.numbers, k = nr_numbers)
+
+print("Here is your password: ")
+
+#combine three list
+result_list = letter_list + symbol_list + number_list
+
+print(result_list)
+
+#convert result_list to string
+
+#randomize the letters of string
