11146efc18485cf36360b8692ca2fc745f479d3b
dani1019 <nihonkita17@gmail.com>
2022-11-16 05:02:45 +0900

    make the game of students_score

---
+++ exercise/students_grade.py
@@ -0,0 +1,13 @@
+name = input("enter the name")
+#enter the name
+#student_scores = {name,score}
+#result → student_grades = {name,grade}
+#Scores 91 - 100: Grade = "Outstanding"
+#Scores 81 - 90: Grade = "Exceeds Expectations"
+#Scores 71 - 80: Grade = "Acceptable"
+#Scores 70 or lower: Grade = "Fail"
+
+student_scores= {'Ronaldo': 92, 'Rooney': 82, 'Messi': 72, 'Beckham' : 62}
+
+for key, value in student_scores.items():
+    #if value > 90 and value <=100:
