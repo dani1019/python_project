231d99f601f371889649e356bf735a64b00ac953
dani1019 <nihonkita17@gmail.com>
2022-11-17 05:29:46 +0900

    study dictionary

---
+++ exercise/multiple_dic.py
@@ -0,0 +1,5 @@
+travel_log = {
+    "France": ["Paris","Lile","Dijon"],
+    "Germany": ["Berlin","Munchen","Frankfurt"]
+}
+#number of visited city
--- exercise/students_grade.py
+++ exercise/students_grade.py
@@ -1,4 +1,3 @@
-name = input("enter the name")
 #enter the name
 #student_scores = {name,score}
 #result → student_grades = {name,grade}
@@ -9,5 +8,16 @@ name = input("enter the name")
 
 student_scores= {'Ronaldo': 92, 'Rooney': 82, 'Messi': 72, 'Beckham' : 62}
 
+student_grades = {}
+
 for key, value in student_scores.items():
-    #if value > 90 and value <=100:
+    if value > 90 and value <=100:
+        student_grades[key] = "Outstanding"
+    elif value > 80 and value <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif value > 70 and value <= 80:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+print(student_grades)
