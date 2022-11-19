3a46b0a4b385ae0c06d5347d3cf9aca91192d44f
dani1019 <nihonkita17@gmail.com>
2022-11-19 19:24:42 +0900

    study dictionary

--- exercise/multiple_dic.py
+++ exercise/multiple_dic.py
@@ -1,5 +1,7 @@
 travel_log = {
-    "France": ["Paris","Lile","Dijon"],
-    "Germany": ["Berlin","Munchen","Frankfurt"]
+    "France": {"cities_visited": ["Paris","Lile","Dijon"],"total_visites":12},
+    "Germany":{"cities_visited": ["Berlin","Munchen","Frankfurt"], "total_visites" : 13}
 }
-#number of visited city
+
+print(travel_log["France"]["total_visites"])
+print(travel_log["Germany"]["total_visites"])
