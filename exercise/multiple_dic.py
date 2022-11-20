2fdbb56d89beb7a65e2bc9bc0a7972c8b528a0de
dani1019 <nihonkita17@gmail.com>
2022-11-20 15:45:56 +0900

    make the exercise of visited country

--- exercise/multiple_dic.py
+++ exercise/multiple_dic.py
@@ -1,7 +1,25 @@
-travel_log = {
-    "France": {"cities_visited": ["Paris","Lile","Dijon"],"total_visites":12},
-    "Germany":{"cities_visited": ["Berlin","Munchen","Frankfurt"], "total_visites" : 13}
+travel_log = [
+{
+    "country" : "France",
+    "visits" : 12,
+    "cities" :["Paris","Lille","Dijon"]
+},
+{
+    "country" : "Germany",
+    "visits" : 5,
+    "cities" : ["Berlin", "Hamburg", "Stuttgart"]
 }
+]
 
-print(travel_log["France"]["total_visites"])
-print(travel_log["Germany"]["total_visites"])
+def add_new_country(country, visit_count,list):
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = visit_count
+    new_country["cities"] = list
+
+    travel_log.append(new_country)
+
+    print(travel_log)
+
+add_new_country("Russia",2,["Moscow", "Saint Peterburg"])
+
