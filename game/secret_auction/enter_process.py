2005f14703cedd87848b998e49eb7a15d6776554
dani1019 <nihonkita17@gmail.com>
2022-11-23 05:40:43 +0900

    make the secret_aution

--- game/secret_auction/enter_process.py
+++ game/secret_auction/enter_process.py
@@ -1,10 +1,10 @@
+import main
 
 def enter_inform():
     user_name= input("What's your name?: ")
     #question user's bid
     user_bid = int(input("What's your bid: "))
     #save user's name and bid
-    global user_info
-    user_info[user_name] = user_bid
-
-    print(user_info)
+    main.user_info[user_name] = user_bid
+    
+    print(main.user_info)
--- game/secret_auction/main.py
+++ game/secret_auction/main.py
@@ -1,4 +1,5 @@
 import enter_process as ep
+from os import system
 
 print("Welcome to the secret auction program")
 #question user's name
@@ -12,8 +13,12 @@ continue_flag = True
 #questsion there is other bidders?
 while continue_flag == True:
     exist_other_bidders = input("Are there any other bidders? Type 'yes' or 'no.'")
+    #if user answer yes, entered infrom is deleted, other user can answer the question
     if exist_other_bidders == "yes":
+        #clear entered inform before
+        system('cls')
         ep.enter_inform()
+
     else:
         print(f"The winner is James with a bid of $142.")
         continue_flag = False
