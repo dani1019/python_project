4fa58afefa4274cefe8e0fd773655352d56e589b
dani1019 <nihonkita17@gmail.com>
2022-12-09 05:41:43 +0900

    write the rule of black_jack

---
+++ game/black_jack/rule.py
@@ -0,0 +1,13 @@
+#the rule of card score
+#queen, king, j is the point of 10
+#Ace is the point of 1 or 11
+#if over 21, can decide which value you want your Ace to represent
+
+#the rule win or lose 
+#1.if both the dealer and I is lower than 21, it is draw
+#2.the sum of three car is over 21 lose
+#if the dealer 21, I am 20, the dealer is winner
+
+#the rule 
+#1.if the sum of dealer's card and my card is lower than 17, should add one card.
+
--- game/calculator/main.py
+++ game/calculator/main.py
@@ -10,10 +10,18 @@ while continue_flag:
     second_number = op.enter_second_number()
     #question user to enter operation
     operation = op.enter_operation()
+
     #question user to enter second number
     #bug 전 연산에서 나온 결과에 다시 연산을 해야하는데,
     #첫 번쨰 입력한 숫자에 연산을 하게 된다.
+    print(f"first_number_1: {first_number}")
     result_number = op.calculate(first_number, second_number, operation)
+
+    #the result of calculation insert first_number
+    first_number = result_number
+
+    print(f"result_number: {result_number}")
+    print(f"first_number_2: {first_number}")
 
     #check if continue to calculate
     #if stop to caculate return false
