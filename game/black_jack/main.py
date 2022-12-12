26f9750d7553134fcadaf5304829cb0123033f43
dani1019 <nihonkita17@gmail.com>
2022-12-13 05:30:15 +0900

    make the game of black_jack

--- game/black_jack/main.py
+++ game/black_jack/main.py
@@ -8,7 +8,8 @@ print(f"your cards: {user_first_cards}")
 print(f"Computer's first card: {computer_first_card}")
 
 #Type 'y' to get another card type 'n' to pass:
-input("Type 'y' to get another card type 'n' to pass:")
+op.add_card(user_first_cards,computer_first_card)
+
 #if typing 'n'
 #ex) your final hand : [9,10]
 #Computer's final hand: [8,10]
--- game/black_jack/operation.py
+++ game/black_jack/operation.py
@@ -1,19 +1,39 @@
 import cards as cd
 import random
 
-#select randomed card
+#operate selecting card
+def select_card(list_of_card, number_of_card):
+    first_cards = random.sample(list_of_card, number_of_card)
+
+    return first_cards
+
+#select first card
 def select_first_card():
     #select the first cards of user
-    user_first_cards = random.choices(cd.cards, k= 2)
+    user_first_cards = select_card(cd.user_cards, 2)
+    print(f"user_first_cards: {user_first_cards}")
+    
     #select the first card of computer
-    computer_first_card = random.choices(cd.cards)
+    computer_first_card = select_card(cd.computer_cards, 1)
+    print(f"computer_first_card: {computer_first_card}")
 
     return user_first_cards,computer_first_card
 
-#def select_one_card():
 
 #if user type 'y', should select the card except to selected first cards
-def add_card():
-    whether_add_card = input("Type 'y' to get another card type 'n' to pass:")
+#select the card not to duplicate 
+def add_card(user_first_cards,computer_first_card):
+    whether_add_card = input("Type 'y' to get another card type 'n' to pass: ")
     if whether_add_card == 'y':
+        select_card(cd.user_cards, 1)
+    else:
+        #print final result
+        print_result(user_first_cards,computer_first_card)
+
+#print final result
+def print_result():
+    print("Your final hand: {user_first_cards}")
+    print("Computer's final hand: {computer_first_card}")
+
+
 
