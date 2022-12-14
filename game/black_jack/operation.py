5cd64290b02c69498a4e235ed6475faacd937ffa
dani1019 <nihonkita17@gmail.com>
2022-12-15 05:09:07 +0900

    modify the game of blackject

--- game/black_jack/operation.py
+++ game/black_jack/operation.py
@@ -1,6 +1,9 @@
 import cards as cd
 import random
 
+having_users_cards = []
+having_computers_cards = []
+
 #operate selecting card
 def select_card(list_of_card, number_of_card):
     first_cards = random.sample(list_of_card, number_of_card)
@@ -10,25 +13,32 @@ def select_card(list_of_card, number_of_card):
 #select first card
 def select_first_card():
     #select the first cards of user
-    user_first_cards = select_card(cd.user_cards, 2)
-    print(f"user_first_cards: {user_first_cards}")
+    user_first_card = select_card(cd.user_cards, 2)
+    #user_first_card add having_users_cards
+    global having_users_cards
+    having_users_cards += user_first_card
+    print(f"user_first_cards: {having_users_cards}")
     
     #select the first card of computer
     computer_first_card = select_card(cd.computer_cards, 1)
-    print(f"computer_first_card: {computer_first_card}")
+    #computer_first_card add having_computers_cards
+    global having_computers_cards
+    having_computers_cards += computer_first_card
+    print(f"computer_first_card: {having_computers_cards}")
 
-    return user_first_cards,computer_first_card
-
+    return having_users_cards,having_computers_cards
 
 #if user type 'y', should select the card except to selected first cards
 #select the card not to duplicate 
-def add_card(user_first_cards,computer_first_card):
+def add_card(having_users_cards,having_computers_cards):
     whether_add_card = input("Type 'y' to get another card type 'n' to pass: ")
     if whether_add_card == 'y':
-        select_card(cd.user_cards, 1)
+        #select one card and add having_users_cards
+        having_users_cards += select_card(cd.user_cards, 1)
+        print(f"users's cards : {having_users_cards}")
     else:
         #print final result
-        print_result(user_first_cards,computer_first_card)
+        print_result(having_users_cards,having_computers_cards)
 
 #print final result
 def print_result():
