05f2d48274da43f5eff4c3a77f90e6794bc453e8
dani1019 <nihonkita17@gmail.com>
2022-12-16 15:24:08 +0900

    modify the game of blackjet

--- game/black_jack/operation.py
+++ game/black_jack/operation.py
@@ -17,14 +17,12 @@ def select_first_card():
     #user_first_card add having_users_cards
     global having_users_cards
     having_users_cards += user_first_card
-    print(f"user_first_cards: {having_users_cards}")
     
     #select the first card of computer
     computer_first_card = select_card(cd.computer_cards, 1)
     #computer_first_card add having_computers_cards
     global having_computers_cards
     having_computers_cards += computer_first_card
-    print(f"computer_first_card: {having_computers_cards}")
 
     return having_users_cards,having_computers_cards
 
@@ -37,13 +35,36 @@ def add_card(having_users_cards,having_computers_cards):
         having_users_cards += select_card(cd.user_cards, 1)
         print(f"users's cards : {having_users_cards}")
     else:
-        #print final result
-        print_result(having_users_cards,having_computers_cards)
+        #if sum of selected card is lower than 17, it should select one card.
+        judge_lower_17(having_users_cards,having_computers_cards)
+        
+
+#if sum of selected card is lower than 17, it should select one card.
+def judge_lower_17(having_users_cards,having_computers_cards):
+    #check if sum of user having cards is lower than 17.
+    if sum(having_users_cards) < 17:
+        having_users_cards += select_card(cd.user_cards, 1)
+        print_final_result(having_users_cards,having_computers_cards)
+    else:
+        print_final_result(having_users_cards,having_computers_cards)
+    
+    #check if sum of computer having cards is lower than 17.
+    if sum(having_computers_cards) < 17:
+        having_computers_cards +=  select_card(cd.computer_cards, 1)
+        print_final_result(having_users_cards,having_computers_cards)
+    else:
+        print_final_result(having_users_cards,having_computers_cards)
 
 #print final result
-def print_result():
-    print("Your final hand: {user_first_cards}")
-    print("Computer's final hand: {computer_first_card}")
+def print_final_result(having_users_cards,having_computers_cards):
+    print(f"Your final hand: {having_users_cards}")
+    print(f"Computer's final hand: {having_computers_cards}")
 
+    if sum(having_users_cards) > sum(having_computers_cards):
+        print("You win.")
+    elif sum(having_users_cards) == sum(having_computers_cards):
+        print("draw.")
+    else:
+        print("You lose.")
 
 
