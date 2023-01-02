d8e6e2d06000d839f4f480150dcb9e2490f1466b
dani1019 <nihonkita17@gmail.com>
2023-01-03 06:02:51 +0900

    make the game of higher_lower

---
+++ game/higher_lower/main.py
@@ -0,0 +1,19 @@
+
+#1.make a key,value name of list = {neymar, cobi,endo}
+#{
+# neymar: [a soccer player, Brazil, 50] ,
+# cobi : [a basketball player, USA, 49]  
+#}
+
+#3.the random selecting compare A
+#select value from key
+
+#4.the random selecting AgainB
+#select value from key
+
+#5.print compare A: neymar , a soccer player, from Brazil
+#6.print Again B : Cobi, a basketball player, from USA
+#7.print Who has more followers Type 'A' or 'B'
+#8-1.if corrected answer will be print you are correct score +5 
+#8-2if wronged answer will be print you are wrong score -5
+#9.until score will be 0, contiune the game, if score is 0, and Game over
