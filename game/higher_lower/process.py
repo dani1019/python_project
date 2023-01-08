8114588919125566f8a370ede8aae01b73a70716
dani1019 <nihonkita17@gmail.com>
2023-01-09 07:17:02 +0900

    modify game of higher

--- game/higher_lower/process.py
+++ game/higher_lower/process.py
@@ -8,24 +8,26 @@ def random_select():
 
 #
 def print_subject(selected_list):
-    comparison_data = []
-    comparison_list = []
+    only_comparison_data = {}
+    comparison_data_list = []
     for index,name in enumerate(selected_list) :
         person_inform = info.inform_list[name]
-        comparison_data.append(name)
-        comparison_data.append(person_inform[2])
-        comparison_list.append(comparison_data)
+        comparison_data_list.append(person_inform[2])
         if index == 0:
             print(f"compare A: {person_inform[0]}, from {person_inform[1]}")
+            comparison_data_list.append("A")
         else:
             print("vs")
-            print(f"Against A: {person_inform[0]}, from {person_inform[1]}")           
+            print(f"Against A: {person_inform[0]}, from {person_inform[1]}")
+            comparison_data_list.append("B")
+    
+        #각 name key에, 각각의 comparison_data_list만 넣도록 수정하기 220110이후 작업
+        only_comparison_data[name] = comparison_data_list
 
     selected_person = input("Who has more followers? Type 'A' or 'B': ")
 
-    return selected_person,comparison_list
+    return selected_person,only_comparison_data
 
-def operate_comparison(selected_person,comparison_list):
-    for element in comparison_list:
-        print(element)
-
+def operate_comparison(selected_person,only_comparison_data):
+    for name, number in only_comparison_data.items():
+        print(name, number)
