24684d25a26cb18162e4ee08b349a87d306da7a0
dani1019 <nihonkita17@gmail.com>
2023-01-10 05:38:45 +0900

    modify the game of higher guess

--- game/higher_lower/process.py
+++ game/higher_lower/process.py
@@ -9,25 +9,31 @@ def random_select():
 #
 def print_subject(selected_list):
     only_comparison_data = {}
-    comparison_data_list = []
+    comparison_data_A = []
+    comparison_data_B = []
     for index,name in enumerate(selected_list) :
         person_inform = info.inform_list[name]
-        comparison_data_list.append(person_inform[2])
+
         if index == 0:
             print(f"compare A: {person_inform[0]}, from {person_inform[1]}")
-            comparison_data_list.append("A")
+            comparison_data_A.append(name)
+            comparison_data_A.append(person_inform[2])
+            only_comparison_data["A"] = comparison_data_A
         else:
             print("vs")
             print(f"Against A: {person_inform[0]}, from {person_inform[1]}")
-            comparison_data_list.append("B")
-    
-        #각 name key에, 각각의 comparison_data_list만 넣도록 수정하기 220110이후 작업
-        only_comparison_data[name] = comparison_data_list
+            comparison_data_B.append(name)
+            comparison_data_B.append(person_inform[2])
+            only_comparison_data["B"] = comparison_data_B
 
     selected_person = input("Who has more followers? Type 'A' or 'B': ")
 
     return selected_person,only_comparison_data
 
 def operate_comparison(selected_person,only_comparison_data):
-    for name, number in only_comparison_data.items():
-        print(name, number)
+    for name, inform in only_comparison_data.items():
+        if selected_person == "A":
+            if only_comparison_data["A"][1] >  only_comparison_data["B"][1]:
+                print(f"you are correct. {only_comparison_data["A"][0]}  is more famous.")
+            else:
+                print("you are wrong.")
