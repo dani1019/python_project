53c96f06fc3e35e0cdd14f60dffff0747b00dd20
dani1019 <nihonkita17@gmail.com>
2023-01-12 05:38:55 +0900

    make the coffee machine

---
+++ game/coffee_machine/main.py
@@ -0,0 +1,10 @@
+
+input("What would you like? (espresso/latte/cappuccino): ")
+#1. if selecting the things you like to drink
+
+#if you enter 'report' print the remain resources(water,milk,coffee,Money)
+#the total resources water : 300ml, milk: 200ml, coffee 100g Money 0
+#the quantity of making things.
+#espresso: 50ml water,18g coffee
+#latte : 200ml water,24g coffee,150ml milk
+#cappuccino: 250ml water,22g coffee,100ml milk
