1090a0f09b940b7ceab25ad8b59978313f4d23f9
dani1019 <nihonkita17@gmail.com>
2023-01-20 05:51:26 +0900

    modify the game of coffee_machine

--- game/coffee_machine/proccess.py
+++ game/coffee_machine/proccess.py
@@ -11,13 +11,20 @@ def order_start():
 
     return selected_menu,number_10_yen,number_100_yen
 
-def operate(selected_menu,number_10_yen,number_100_yen):
+def select_menu(selected_menu,number_10_yen,number_100_yen):
     #caculate the remanied quantity of coffee machince - usered quantity
     if selected_menu == "report":
         pass
     else:
         #if user select espresso, execute calculating
-        if selected_menu == "e":
+        if selected_menu == "espresso":
+            calcute_quantity("espresso")
+        #if user select latte, execute calculating
+        elif selected_menu == "latte":
+            calcute_quantity("latte")
+        #if user select cappuccino, execute calculating
+        else:
+            calcute_quantity("cappuccino")
             lst.coffee_machine["water"] - lst.kind_of_drink["espresso"]["water"]
             lst.coffee_machine["coffee"] - lst.kind_of_drink["espresso"]["coffee"]
             lst.coffee_machine["milk"] - lst.kind_of_drink["espresso"]["milk"]
@@ -27,6 +34,12 @@ def operate(selected_menu,number_10_yen,number_100_yen):
             print(lst.coffee_machine["coffee"])
             print(lst.coffee_machine["milk"])
             print(lst.coffee_machine["coin"])
+
+def calcute_quantity():
+    for index,matarial in enumerate(lst.coffee_machine):
+        print(coffee_machine[index])
+
+
 
 
             
