028668aa42f3e2876a3fc153e4e56e874b6c9ba7
dani1019 <nihonkita17@gmail.com>
2023-02-01 05:04:22 +0900

    modify the quiz

--- game/quiz-game-start/quiz_brain.py
+++ game/quiz-game-start/quiz_brain.py
@@ -6,8 +6,36 @@
 class Question_Opearte:
     def __init__(self, question_bank):
         self.question_bank = question_bank
+        self.correct_number = 0
+        self.len_question_bank = len(question_bank) - 1
+
+    for index,element in enumerate(question_bank):
+        q_text = question_bank[index].text
+        q_answer = question_bank[index].answer
+
+        print(f"q_text: {q_text}")
+        print(f"q_answer: {q_answer}")
+        
+        #print question and require answer to user
+        select_boolean= input(f"Q.{index + 1}: {q_text} (True/False)?:")
+        #check whether answer is correct
+        check_answer_correct(select_boolean,q_answer)
+
+        #print result
+        if index == self.len_question_bank:
+            print_result()
     
-        for index,element in enumerate(question_bank):
-            print(question_bank[index].text)
+    def check_answer_correct(select_boolean,q_answer):
+        if select_boolean == q_answer:
+            self.correct_number += 1
+    
+    def print_result():
+        print("You are correct {self.correct_number}")
+    
+
+
+
+            
+            
 
 
